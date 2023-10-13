341c5768b6565a130157838da5cef676431d6ff2
nadiaseleem <nadiaseleem97@gmail.com>
2023-10-13 14:20:30 +0300

    + add app bar + add bottom navigation view

--- app/src/main/java/com/example/routee_commerce/MainActivity.kt
+++ app/src/main/java/com/example/routee_commerce/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.routee_commerce
 
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        makeStatusBarTransparentAndIconsClear()
+
     }
+
+    private fun makeStatusBarTransparentAndIconsClear() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR)
+            window.statusBarColor = Color.TRANSPARENT
+        }
+    }
+
+
 }
