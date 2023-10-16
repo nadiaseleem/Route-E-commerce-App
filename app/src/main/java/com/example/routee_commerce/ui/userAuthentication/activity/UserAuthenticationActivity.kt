1c09e9acdedf29ec4170120ebadbfe8a4da00dcb
ziad328 <ziad32seleem@gmail.com>
2023-10-16 21:53:03 +0300

    + Register Fragment design + UserAuthenticationActivity + login,Register Fragments + some strings

---
+++ app/src/main/java/com/example/routee_commerce/ui/userAuthentication/activity/UserAuthenticationActivity.kt
@@ -0,0 +1,12 @@
+package com.example.routee_commerce.ui.userAuthentication.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.routee_commerce.R
+
+class UserAuthenticationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_authentication)
+    }
+}
---
+++ app/src/main/java/com/example/routee_commerce/ui/userAuthentication/fragments/LoginFragment.kt
@@ -0,0 +1,18 @@
+package com.example.routee_commerce.ui.userAuthentication.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.routee_commerce.R
+
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+}
